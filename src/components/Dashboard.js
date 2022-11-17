c4745b2595f9ddf4340490acc068d365143a58bd
Valcokoro <valcokoro@yahoo.com>
2022-11-17 14:56:18 -0800

    Added to the Dashbord Component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,9 @@ import '../styles/componentstyle/Dashboard.css';
 import search from '../assets/search.svg';
 import bell from '../assets/bell.svg';
 import overview from '../assets/overview.svg';
+import wallet from '../assets/wallet.png';
+import payment from '../assets/payment.png';
+import debt from '../assets/debt.png';
 
 const Dashboard = () => {
   return (
@@ -24,7 +27,25 @@ const Dashboard = () => {
         </form>
         <div className='side-menu'>
           <button className='invoice-btn'>GENERATE INVOICE</button>
-          <a className='main'>Main</a>
+          <a href='#' className='main'>Main</a>
+          <a href='#'>
+            <img src={overview} alt='overview icon' className='overview-icon'/>
+            <span className='overview'>Overview</span>
+          </a>
+          <a href='#' className='payments'>Payments</a>
+          <a href='#'>
+            <img src={wallet} alt='wallet icon' className='wallet-icon'/>
+            <span className='all-payments'>All Payments</span>
+          </a>
+          <a href='#' className='reconciled'>
+            <img src={payment} alt='payment icon' className='payment-icon'/>
+            <span className='rec-payments'>Reconcilled Payments</span>
+          </a>
+          <a href='#' className='unreconciled'>
+            <img src={debt} alt='debt icon' className='debt-icon'/>
+            <span className='debt'>Un-Reconcilled Payments</span>
+          </a>
+          
 
         </div>
             
