a984695bec762a81204fa8e5ac86d47b68accf0d
Valcokoro <valcokoro@yahoo.com>
2022-11-17 05:32:48 -0800

    Building Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/componentstyle/Dashboard.css';
 import search from '../assets/search.svg';
+import bell from '../assets/bell.svg';
+import overview from '../assets/overview.svg';
 
 const Dashboard = () => {
   return (
@@ -10,8 +12,23 @@ const Dashboard = () => {
             <button className='search-btn'>
                 <img src={search} alt='search icon' className='search-icon'/>
             </button>
-
+            <input 
+               type='text' 
+               value=''
+               className='search-bar'
+               placeholder='Search...'
+            />
+            <a href='#' className='support'>Support</a>
+            <a href='#' className='faq'>FAQ</a>
+            <img src={bell} alt='bell icon' className='bell-icon'/>
         </form>
+        <div className='side-menu'>
+          <button className='invoice-btn'>GENERATE INVOICE</button>
+          <a className='main'>Main</a>
+
+        </div>
+            
+        
     </div>
   )
 }
